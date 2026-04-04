40d0726a7e5b95eba64acc23e3cfe87635621bea
Christoph Kappestein <christoph.kappestein@gmail.com>
2026-04-05 00:37:24 +0200

    handle not supported http method

--- public/index.php
+++ public/index.php
@@ -10,6 +10,7 @@ $action = \ltrim($_SERVER['PATH_INFO'] ?? '', '/');
 $worker = new \Fusio\Worker\Worker();
 
 try {
+    $code = 200;
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         $response = $worker->get();
     } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -20,16 +21,23 @@ try {
         $response = $worker->put($action, $payload);
     } elseif ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
         $response = $worker->delete($action);
+    } else {
+        $code = 405;
+        $response = newErrorMessage('HTTP method not allowed');
     }
 
-    \http_response_code(200);
+    \http_response_code($code);
     echo \json_encode($response);
 } catch (\Throwable $e) {
+    \http_response_code(500);
+    echo \json_encode(newErrorMessage($e->getMessage(), $e->getTraceAsString()));
+}
+
+function newErrorMessage(string $message, ?string $trace = null): Message
+{
     $return = new Message();
     $return->setSuccess(false);
-    $return->setMessage($e->getMessage());
-    $return->setTrace($e->getTraceAsString());
-
-    \http_response_code(500);
-    echo \json_encode($return);
+    $return->setMessage($message);
+    $return->setTrace($trace);
+    return $return;
 }
